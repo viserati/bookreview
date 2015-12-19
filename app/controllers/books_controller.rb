14ae8bc2de37fc97303bbec730814cfa3dbe4dcb
Keith Turner <turner@keithturner.com>
2015-12-18 23:18:33 -0700

    Part 3 Add Paperclip Gem and book images

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -51,7 +51,7 @@ def index
   private
 
   def book_params
-    params.require(:book).permit(:title, :description, :author, :category_id)
+    params.require(:book).permit(:title, :description, :author, :category_id, :book_img)
   end
 
   def find_book

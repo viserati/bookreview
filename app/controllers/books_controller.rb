276146dab9af52da21a8850a8f55396f9157aef7
Keith Turner <turner@keithturner.com>
2016-02-02 19:21:05 -0700

    Part 4 Add Raty JQuery Stars and User Authentication.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
 before_action :find_book, only: [:show, :edit, :update, :destroy]
+before_action :authenticate_user!, only: [:new, :edit]
 
 def index
   if params[:category].blank?
@@ -12,6 +13,11 @@ def index
   end
 
   def show
+    if @book.reviews.blank?
+      @average_review = 0
+    else
+      @average_review = @book.reviews.average(:rating).round(2)
+    end
   end
 
   def new
